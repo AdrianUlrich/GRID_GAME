484a85ff65bb3f0224464b00f2bea132d49b9dd9
dfrei <dylan.frei@epfl.ch>
2018-11-29 20:16:06 +0100

    x < 4.3

--- cs107-mini-projet2/java/ch/epfl/cs107/play/game/areagame/Area.java
+++ cs107-mini-projet2/java/ch/epfl/cs107/play/game/areagame/Area.java
@@ -5,125 +5,199 @@ import ch.epfl.cs107.play.game.actor.Actor;
 import ch.epfl.cs107.play.game.areagame.actor.Interactable;
 import ch.epfl.cs107.play.io.FileSystem;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Transform;
+import ch.epfl.cs107.play.math.Vector;
 import ch.epfl.cs107.play.window.Keyboard;
 import ch.epfl.cs107.play.window.Window;
 import ch.epfl.cs107.play.game.areagame.actor.Interactable;
 
+import java.util.LinkedList;
 import java.util.List;
 
-
 /**
- * Area is a "Part" of the AreaGame. It is characterized by its AreaBehavior and a List of Actors
+ * Area is a "Part" of the AreaGame. It is characterized by its AreaBehavior and
+ * a List of Actors
  */
 public abstract class Area implements Playable {
 
-    // Context objects
-    // TODO implements me #PROJECT #TUTO
+	// Context objects
+	private Window window;
+	private FileSystem fileSystem;
 
-	/** @return (float): camera scale factor, assume it is the same in x and y direction */
-    public abstract float getCameraScaleFactor();
-    
-    /**
-     * Add an actor to the actors list
-     * @param a (Actor): the actor to add, not null
-     * @param forced (Boolean): if true, the method ends
-     */
-    private void addActor(Actor a, boolean forced) {
-        // TODO implements me #PROJECT #TUTO
-    }
+	/// List of Actors inside the area
+	private List<Actor> actors;
+	private List<Actor> registeredActors;
+	private List<Actor> unregisteredActors;
+	
+	// Camera Parameter
+	// actor on which the view is centered
+	private Actor viewCandidate ;
+	// effective center of the view
+	private Vector viewCenter ;
+	
+	// TODO implements me #PROJECT #TUTO
 
-    /**
-     * Remove an actor form the actor list
-     * @param a (Actor): the actor to remove, not null
-     * @param forced (Boolean): if true, the method ends
-     */
-    private void removeActor(Actor a, boolean forced){
-        // TODO implements me #PROJECT #TUTO
-    }
+	/**
+	 * @return (float): camera scale factor, assume it is the same in x and y
+	 *         direction
+	 */
+	public abstract float getCameraScaleFactor();
 
-    /**
-     * Register an actor : will be added at next update
-     * @param a (Actor): the actor to register, not null
-     * @return (boolean): true if the actor is correctly registered
-     */
-    public final boolean registerActor(Actor a){
-        // TODO implements me #PROJECT #TUTO
-        return false;
-    }
+	/**
+	 * Add an actor to the actors list
+	 * 
+	 * @param a      (Actor): the actor to add, not null
+	 * @param forced (Boolean): if true, the method ends
+	 */
+	private void addActor(Actor a, boolean forced) {
+		// Here decisions at the area level to decide if an actor
+		// must be added or not
+		boolean errorOccured = !actors.add(a);
+		if (errorOccured && !forced) {
+			System.out.println("Actor " + a + " cannot be completely added , so remove it from where it was");
+			removeActor(a, true);
+		}
+	}
 
-    /**
-     * Unregister an actor : will be removed at next update
-     * @param a (Actor): the actor to unregister, not null
-     * @return (boolean): true if the actor is correctly unregistered
-     */
-    public final boolean unregisterActor(Actor a){
-        // TODO implements me #PROJECT #TUTO
-        return false;
-    }
+	/**
+	 * Remove an actor form the actor list
+	 * 
+	 * @param a      (Actor): the actor to remove, not null
+	 * @param forced (Boolean): if true, the method ends
+	 */
+	private void removeActor(Actor a, boolean forced) {
+		boolean errorOccured = !actors.remove(a);
+		if (errorOccured && !forced) {
+			System.out.println("Actor " + a + " cannot be completely removed , so added it where it was");
+			addActor(a, true);
+		}
+	}
 
-    /**
-     * Getter for the area width
-     * @return (int) : the width in number of cols
-     */
-    public final int getWidth(){
-        // TODO implements me #PROJECT #TUTO
-        return 0;
-    }
+	/**
+	 * Register an actor : will be added at next update
+	 * 
+	 * @param a (Actor): the actor to register, not null
+	 * @return (boolean): true if the actor is correctly registered
+	 */
+	public final boolean registerActor(Actor a) {
+		// TODO implements me #PROJECT #TUTO
+		registeredActors.add(a);
+		return false;
+	}
 
-    /**
-     * Getter for the area height
-     * @return (int) : the height in number of rows
-     */
-    public final int getHeight(){
-        // TODO implements me #PROJECT #TUTO
-        return 0;
-    }
+	/**
+	 * Unregister an actor : will be removed at next update
+	 * 
+	 * @param a (Actor): the actor to unregister, not null
+	 * @return (boolean): true if the actor is correctly unregistered
+	 */
+	public final boolean unregisterActor(Actor a) {
+		// TODO implements me #PROJECT #TUTO
+		unregisteredActors.add(a);
+		return false;
+	}
 
-    /** @return the Window Keyboard for inputs */
-    public final Keyboard getKeyboard () {
-        // TODO implements me #PROJECT #TUTO
-        return null;
-    }
+	/**
+	 * Getter for the area width
+	 * 
+	 * @return (int) : the width in number of cols
+	 */
+	public final int getWidth() {
+		// TODO implements me #PROJECT #TUTO
+		return 0;
+	}
 
-    /// Area implements Playable
+	/**
+	 * Getter for the area height
+	 * 
+	 * @return (int) : the height in number of rows
+	 */
+	public final int getHeight() {
+		// TODO implements me #PROJECT #TUTO
+		return 0;
+	}
 
-    @Override
-    public boolean begin(Window window, FileSystem fileSystem) {
-        // TODO implements me #PROJECT #TUTO
-        return true;
-    }
+	/** @return the Window Keyboard for inputs */
+	public final Keyboard getKeyboard() {
+		// TODO implements me #PROJECT #TUTO
+		return null;
+	}
 
-    /**
-     * Resume method: Can be overridden
-     * @param window (Window): display context, not null
-     * @param fileSystem (FileSystem): given file system, not null
-     * @return (boolean) : if the resume succeed, true by default
-     */
-    public boolean resume(Window window, FileSystem fileSystem){
-        return true;
-    }
+	/// Area implements Playable
 
-    @Override
-    public void update(float deltaTime) {
-        // TODO implements me #PROJECT #TUTO
-    }
+	@Override
+	public boolean begin(Window window, FileSystem fileSystem) {
+		// TODO implements me #PROJECT #TUTO
+		this.fileSystem = fileSystem;
+		this.window = window;
+		actors = new LinkedList<>();
+		viewCenter =  Vector.ZERO;
+		viewCandidate = null;
+		return true;
+	}
 
+	/**
+	 * Resume method: Can be overridden
+	 * 
+	 * @param window     (Window): display context, not null
+	 * @param fileSystem (FileSystem): given file system, not null
+	 * @return (boolean) : if the resume succeed, true by default
+	 */
+	public boolean resume(Window window, FileSystem fileSystem) {
+		return true;
+	}
 
-    private void updateCamera () {
-        // TODO implements me #PROJECT #TUTO
-    }
+	@Override
+	public void update(float deltaTime) {
+		// TODO implements me #PROJECT #TUTO
+		updateCamera();
+		purgeRegistration();
+		actors.forEach((actor) -> {
+			actor.draw(window);
+			actor.update(deltaTime);
+		});
+	}
 
-    /**
-     * Suspend method: Can be overridden, called before resume other
-     */
-    public void suspend(){
-        // Do nothing by default
-    }
+	private final void purgeRegistration() {
+		// add newly registered actors to the actors list
+		registeredActors.forEach((actor) -> {
+			addActor(actor, false);
+		});
+		// remove unwanted actors from the actor list
+		unregisteredActors.forEach((actor) -> {
+			removeActor(actor, false);
+		});
+		// once updated actors, clears lists
+		registeredActors.clear();
+		unregisteredActors.clear();
+	}
 
+	private void updateCamera() {
+		// TODO implements me #PROJECT #TUTO
+		if (viewCandidate != null) {
+			viewCenter = viewCandidate.getPosition();
+		}
+		// Compute new viewport
+		// TODO find the right scale factor
+		Transform viewTransform = Transform.I.scaled(1).translated(viewCenter) ;
+		window.setRelativeTransform(viewTransform) ;
+	}
+	
+	public final void setViewCandidate(Actor a){
+		this.viewCandidate = a ;
+		}
 
-    @Override
-    public void end() {
-        // TODO save the AreaState somewhere
-    }
+	/**
+	 * Suspend method: Can be overridden, called before resume other
+	 */
+	public void suspend() {
+		// Do nothing by default
+		purgeRegistration();
+	}
+
+	@Override
+	public void end() {
+		// TODO save the AreaState somewhere
+	}
 
 }
--- cs107-mini-projet2/java/ch/epfl/cs107/play/game/areagame/AreaBehavior.java
+++ cs107-mini-projet2/java/ch/epfl/cs107/play/game/areagame/AreaBehavior.java
@@ -1,25 +1,45 @@
 package ch.epfl.cs107.play.game.areagame;
 
+import ch.epfl.cs107.play.game.areagame.io.ResourcePath;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.window.Image;
 import ch.epfl.cs107.play.window.Window;
 
 /**
  * AreaBehavior manages a map of Cells.
  */
-public abstract class AreaBehavior
-{
+public abstract class AreaBehavior {
 
-    /// The behavior is an Image of size height x width
-    // TODO implements me #PROJECT #TUTO
+	/// The behavior is an Image of size height x width
+	private final Image behaviorMap;
+	private final int width, height;
+	/// We will convert the image into an array of cells
+	private final Cell[][] cells;
 
-    /**
-     * Default AreaBehavior Constructor
-     * @param window (Window): graphic context, not null
-     * @param fileName (String): name of the file containing the behavior image, not null
-     */
-    public AreaBehavior(Window window, String fileName){
-        // TODO implements me #PROJECT #TUTO
-    }
+	// TODO implements me #PROJECT #TUTO
 
-    // TODO implements me #PROJECT #TUTO
+	/**
+	 * Default AreaBehavior Constructor
+	 * 
+	 * @param window   (Window): graphic context, not null
+	 * @param fileName (String): name of the file containing the behavior image, not
+	 *                 null
+	 */
+	public AreaBehavior(Window window, String fileName) {
+		// TODO implements me #PROJECT #TUTO
+		behaviorMap = window.getImage(ResourcePath.getBehaviors(fileName), null, false);
+		width = behaviorMap.getWidth();
+		height = behaviorMap.getHeight();
+		cells = new Cell[width][height];
+	}
+
+	// TODO implements me #PROJECT #TUTO
+	public abstract class Cell {
+		DiscreteCoordinates pos;
+
+		public Cell(int x, int y) {
+			pos = new DiscreteCoordinates(x, y);
+		}
+	}
 
 }
